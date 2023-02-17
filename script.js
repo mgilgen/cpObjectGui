7a9cdf2aae4a7b583991866768f30a09bb39c5ad
mgilgen <mike.gilgen@students.fhnw.ch>
2023-02-17 10:20:00 +0100

    initial

---
+++ script.js
@@ -0,0 +1,6 @@
+function changeColorIn(object){
+    object.setAttribute("class", "p-3 m-5 bg-primary-subtle");
+}
+function changeColorOut(object){
+    object.setAttribute("class", "p-3 m-5 bg-primary");
+}
